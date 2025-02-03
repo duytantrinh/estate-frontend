ac435ede71fd6cce38f6447511d26de6db996a42
Duy Tan Trinh <trinhtan1288@gmail.com>
2025-02-02 19:57:52 -0500

    second commit

--- src/routes/profileUpdatePage/ProfileUpdatePage.jsx
+++ src/routes/profileUpdatePage/ProfileUpdatePage.jsx
@@ -3,7 +3,7 @@ import "./ProfileUpdatePage.scss"
 import {AuthContext} from "../../context/AuthContext"
 import apiRequest from "../../lib/apiRequest"
 import {useNavigate} from "react-router-dom"
-import UploadWidget from "../../components/uploadWidget/uploadWidget"
+import UploadWidget from "../../components/uploadWidget/UploadWidget"
 
 function ProfileUpdatePage() {
   const {currentUser, updateUser} = useContext(AuthContext)
